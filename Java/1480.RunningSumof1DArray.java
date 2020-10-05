93f38650577169be693f93e23480611c85a65fe0
Atin <atingarg1406@gmail.com>
2020-10-06 01:41:32 +0530

    RunningSumof1DArray

--- Java/1480.RunningSumof1DArray.java
+++ Java/1480.RunningSumof1DArray.java
@@ -1,8 +1,3 @@
-//We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i])
-//Example: Input: nums = [1,2,3,4]
-//         Output: [1,3,6,10]
-//         Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
-
 public class Solution {
     
     public int[] runningSum(int[] nums) {
@@ -12,3 +7,8 @@ public class Solution {
         return nums;
     }
 }
+
+//We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i])
+//Example: Input: nums = [1,2,3,4]
+//         Output: [1,3,6,10]
+//         Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
