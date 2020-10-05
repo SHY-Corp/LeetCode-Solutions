ce19bb5f4a01f62f657bb20e4ddd195ed617efd3
AryashDubey <dubeyaryash@gmail.com>
2020-10-06 00:52:38 +0530

    Added Python Sum Of Two Numbers

---
+++ Python/PythonSum.py
@@ -0,0 +1,4 @@
+def sum(a,b):
+    print("The Answer Is :- " ,a+b)
+sum(int(input("Enter Your First Number:- ")),int(input("Enter Your Second Number:- "))) #int(input()) asks for a input in the form of an Integer
+ 
