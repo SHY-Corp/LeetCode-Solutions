67bfab9f121da6990cf8770d493389e3a757fd8f
AryashDubey <70095055+AryashDubey@users.noreply.github.com>
2020-10-06 00:57:31 +0530

    Update PythonSum.py

--- Python/PythonSum.py
+++ Python/PythonSum.py
@@ -1,3 +1,4 @@
+#This Program Will Make the user entert Two Numbers And Will Print The Sum
 def sum(a,b):
     print("The Answer Is :- " ,a+b)
 sum(int(input("Enter Your First Number:- ")),int(input("Enter Your Second Number:- "))) #int(input()) asks for a input in the form of an Integer
