3a1df235caabf7ca1b332405b3a1c33b399dbb17
patelviralb <vpatel@quickbase.com>
2020-10-07 17:24:39 -0400

    Minor update

--- Java/647. Palindromic Substrings.java
+++ Java/647. Palindromic Substrings.java
@@ -1,7 +1,7 @@
 /**
  * Question:
  * Given a string, your task is to count how many palindromic substrings in this string.
- *<p>
+ * <p>
  * The substrings with different start indexes or end indexes are counted as different substrings even they consist of
  * same characters.
  * <p>
