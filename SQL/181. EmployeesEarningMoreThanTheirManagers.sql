88f3d2dacb7a958ed5d28423f649e23d6c4406b8
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-10-07 11:19:25 +0530

    Delete 181. EmployeesEarningMoreThanTheirManagers.sql

--- SQL/181. EmployeesEarningMoreThanTheirManagers.sql
+++
@@ -1,4 +0,0 @@
-Select a.Name as Employee                 /* Selecting column and renaming it */
-from Employee as a join employee as b     /* Self join using 2 different names */
-on a.ManagerId=b.Id                       /* Join condition */
-and a.Salary>b.Salary;                    /* Condition for selecting records */
