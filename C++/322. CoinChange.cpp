543f943a5b5b19108e079662c533ff60e6b27fdb
agrawaladitya192 <agrawaladitya192@gmail.com>
2020-10-07 17:07:03 +0530

    Added Problem Statement and Sample I/O in Problem 322

--- C++/322. CoinChange.cpp
+++ C++/322. CoinChange.cpp
@@ -1,3 +1,14 @@
+"""
+Problem Statement:
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. 
+If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin.
+Sample Input:
+coins = [1,2,5], amount = 11
+Output : 3
+Explanation : 11 = 5 + 5 + 1
+"""
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
