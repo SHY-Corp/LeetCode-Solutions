3141a89a8d28b3e7efc130254497d03224dbb167
DCCollao <dlcollao@uc.cl>
2020-10-07 01:25:00 -0300

    fixed two sum problem.py :bug:

--- Python/1.TwoSum.py
+++ Python/1.TwoSum.py
@@ -1,4 +1,8 @@
 class Solution:
+    """
+    This program takes a list and returns 
+    indices of the two numbers such that they add up to target 
+    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashes = dict()
         for i, num in enumerate(nums):
