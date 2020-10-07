530287678bbde60add6d97109f4bc85d5bcbdefc
abhi2694 <abhi2694@gmail.com>
2020-10-07 16:33:01 +0530

    Added proposed solution and problem

--- Java/124.BinaryTreeMaxSumPath.java
+++ Java/124.BinaryTreeMaxSumPath.java
@@ -1,4 +1,36 @@
-/**
+/*
+For the given binary tree one needs to find Maximum sum path,a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+Example 1:
+
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6
+Example 2:
+
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
+Proposed Solution:
+For each node there can be four ways that the max path goes through the node:
+1. Node only
+2. Max path through Left Child + Node
+3. Max path through Right Child + Node
+4. Max path through Left Child + Node + Max path through Right Child
+
+The idea is to keep trace of four paths and pick up the max one in the end.
+*/
+/*
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
