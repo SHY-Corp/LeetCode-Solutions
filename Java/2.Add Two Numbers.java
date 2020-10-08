fd39aca037c0b81252bd245f6bc23c129f6ad1bf
Hanning Lin <liam.hanninglin@outlook.com>
2020-10-08 09:21:28 -0700

    2.addTwoNumbers.java done

---
+++ Java/2.Add Two Numbers.java
@@ -0,0 +1,40 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // O(n) O(n)
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        int sum = 0;
+        ListNode cur = dummy;
+        ListNode p1 = l1, p2 = l2;
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
+        }
+        if (sum == 1) {
+            cur.next = new ListNode(1);
+        }
+        return dummy.next;
+        
+        
+    }
+}
