fd5253d231d444804d826373eaa781384a2e7a77
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-10-08 12:47:24 +0530

    Update and rename ValidPerfectSquare.py to 367ValidPerfectSquare.py
    issue: 367 | Valid Perfect Square | Python #204 
    Solved

--- Python/367ValidPerfectSquare.py
+++ Python/367ValidPerfectSquare.py
@@ -1,6 +1,9 @@
-Python 4 Methods with time testing
+#Problem Statement: Given a positive integer num, write a function which returns True if num is a perfect square else False.
+ #   Link: https://leetcode.com/problems/valid-perfect-square/
 
-1. Solving with Bitwise trick.
+#Python 4 Methods with time testing
+
+#1. Solving with Bitwise trick.
 
     def BitwiseTrick(self, num):
        root = 0
@@ -13,14 +16,14 @@ Python 4 Methods with time testing
            bit >>= 1        
        return root * root == num
 
-2. Using Newton's Method
+#2. Using Newton's Method
 
     def NewtonMethod(self, num):
         r = num
         while r*r > num:
             r = (r + num/r) // 2
         return r*r == num
-3. Math Trick for Square number is 1+3+5+ ... +(2n-1)
+#3. Math Trick for Square number is 1+3+5+ ... +(2n-1)
 
     def Math(self, num):
         i = 1
@@ -28,7 +31,7 @@ Python 4 Methods with time testing
             num -= i
             i += 2       
         return num == 0
-4. Binary Search Method
+#4. Binary Search Method
 
     def BinarySearch(self, num):
         left = 0
@@ -44,9 +47,9 @@ Python 4 Methods with time testing
                 left = mid +1
         return False
         
-I test these Four methods with for i in range(100000): function(i), and get results as below.
+#I test these Four methods with for i in range(100000): function(i), and get results as below.
 
-Time for Bitwise's Method : 0.45249104499816895
-Time for Newton's Method : 0.3492701053619385
-Time for Math's Method : 2.641957998275757
-Time for Binary Search's Method : 1.5031492710113525
+#Time for Bitwise's Method : 0.45249104499816895
+#Time for Newton's Method : 0.3492701053619385
+#Time for Math's Method : 2.641957998275757
+#Time for Binary Search's Method : 1.5031492710113525
