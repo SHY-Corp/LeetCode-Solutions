60f8dee67d6e869820b203ba77a4c15deceded80
Smriti Naik <64346825+shruti1421@users.noreply.github.com>
2020-10-09 02:48:51 +0530

    Create 50. Pow(x, n).cpp

---
+++ C++/50. Pow(x, n).cpp
@@ -0,0 +1,17 @@
+/*To implement pow(x, n), which calculates x raised to the power n (i.e. xn).
+ 
+
+For Example:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000*/
+
+class Solution {
+public:
+    
+    double myPow(double x, int n)
+    {
+        double c=n;
+        return pow(x,c);  // using pow function to get x^n
+    }
+};
