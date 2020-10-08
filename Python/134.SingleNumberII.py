51e06bbc0d33ce941393f365e6a29de80416dc46
= <=>
2020-10-08 21:39:52 +0530

    Changed Program in 134.SingleNumberII.py

--- Python/134.SingleNumberII.py
+++ Python/134.SingleNumberII.py
@@ -1,15 +1,44 @@
 #Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
-class Solution(object):
-    def singleNumber(self, nums):
-        single = 0
-        original = 0
-        #First set function is used to find all the unqiue elements and then they are added
-        for i in set(nums):
-            single +=i
-        #Then all the elements of the given list are added    
+#-----------------------Test Example-----------------------
+#Input = 1,1,3,1
+#Output = 3
+#-----------------------Test Example2-----------------------
+#Input = 0,1,0,1,0,1,9
+#Output = 9
+
+"""
+This Code checks the number from array that appears only once using dictionary.
+Time Complexity  = O(n) i.e. Linear Time Complexity
+"""
+
+
+class Solution:
+    def find(self, nums):
+        count = dict()
+        #counting appearance of each number in array and updating the count
         for i in nums:
-            original +=i
-        #Variable single conmtains the sum of all unique elements and variable original contains the sum of all elements in the given list.Multiplying the sum of all the unique
-        #elements by 3 and subtracting it with the sum of all the elements in the original list will leave us with the sum of the single number taken twice. Hence we divide by 2 
-        # to get the single number
-        return ((single * 3) - original)/2
+            count[i] = count.get(i,0) + 1
+        
+        # getting the number from dictionary that appears once
+        for key,value in count.items():
+            if value == 1:
+                return key
+
+#taking input from user of all array elements  
+nums = list()
+c = 0
+print("Enter Numbers in the array")
+print("When done with entering numbers enter \"q\"")
+while c == 0:
+    n = input()
+    if n == 'q' or n == 'Q':
+        break
+    try:
+        n = int(n)
+    except:
+        print("Enter Numbers only")
+        continue
+    nums.append(n)
+obj = Solution() #class object
+print("\n")
+print(obj.find(nums), "appears once") #function call
