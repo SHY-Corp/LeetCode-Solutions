2ee5f0a4d89e58b06af3ee597b4e192acbbef0f0
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2020-10-09 19:47:15 +0100

    Add 941. Valid Mountain Array Solution in Python

---
+++ Python/941. Valid Mountain Array.py
@@ -0,0 +1,20 @@
+#941. Valid Mountain Array problem is described here : https://leetcode.com/problems/valid-mountain-array/
+
+class Solution(object):
+    def validMountainArray(self, A):
+        N = len(A)
+        i = 0
+
+        # walk up
+        while i+1 < N and A[i] < A[i+1]:
+            i += 1
+
+        # peak can't be first or last
+        if i == 0 or i == N-1:
+            return False
+
+        # walk down
+        while i+1 < N and A[i] > A[i+1]:
+            i += 1
+
+        return i == N-1
