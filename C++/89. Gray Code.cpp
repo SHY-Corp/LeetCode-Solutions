cece40ac5f31d69ec124b80fd1fd0992a535acf7
gaurav1620 <gauravak007@gmail.com>
2020-10-09 10:29:05 +0530

    Update 89. Gray Code

--- C++/89. Gray Code.cpp
+++ C++/89. Gray Code.cpp
@@ -1,3 +1,19 @@
+/*
+The gray code is a binary numeral system where two successive values differ in only one bit.
+Given a non-negative integer n representing the total number of bits in the code, 
+print the sequence of gray code. A gray code sequence must begin with 0.
+
+Example :
+Input: 2
+Output: [0,1,3,2]
+Explanation:
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
