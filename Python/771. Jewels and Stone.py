de4c9d8181d8f4eaa60f5ca192f4af624bf13b2b
Siddharth Gangwar <gangwarsiddharth25@gmail.com>
2020-10-09 21:47:39 +0530

    Added Jewels and Stones problem in Python

--- Python/771. Jewels and Stone.py
+++ Python/771. Jewels and Stone.py
@@ -1,3 +1,10 @@
+'''
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have. 
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are should be distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+'''
 class Solution(object):
    def numJewelsInStones(self, J, S):
       jewels = {}
