a9c52d20e62425aaedb34e600e6a54917d1f499a
Cheshtaa Agarrwal <56514142+cheshtaaagarrwal@users.noreply.github.com>
2020-10-09 11:39:24 +0530

    Update 142_linkedlist_cycle_2.java
    Added comments.

--- Java/142_linkedlist_cycle_2.java
+++ Java/142_linkedlist_cycle_2.java
@@ -1,4 +1,8 @@
- public ListNode detectCycle(final ListNode head) {
+//Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+//Notice that you should not modify the linked list.
+//There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+public ListNode detectCycle(final ListNode head) {
         if (head == null || head.next == null)
             return head;
 
