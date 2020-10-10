f0627032d64602b9e82e9b6d3851696704b37ba9
Largetable <53430360+Largetable@users.noreply.github.com>
2020-10-10 16:38:43 +0100

    Update and rename 136Single number.py to 136.Single number.py

--- Python/136Single number.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-class Solution(object):
-    def singleNumber(self, nums):
-        c=Counter(nums)
-        sorted_items=sorted(c.items(), key = lambda x:x[1])
-        return sorted_items[0][0]
-        
