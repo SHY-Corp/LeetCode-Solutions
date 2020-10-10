c984a684711e16d138e9984579efd8aaa8ccf147
Uday <iamuday1618@gmail.com>
2020-10-10 23:45:29 +0530

    Implement Factorial Trailing Zeroes

--- Python/172. FactorialTrailingZeroes.py
+++ Python/172. FactorialTrailingZeroes.py
@@ -9,9 +9,9 @@ Explanation: 5! = 120, one trailing zero.
 
 # Algorithm explained:
 #   In the expanded form of facotorial, the occurenses of 5 or its multiples
-# contribute to the trailing zeros. So, first we count the multiples of 5,
+# contribute to the trailing zeroes. So, first we count the multiples of 5,
 # and now we account for the powers of 5, which can be written as
-# repeated multiplication of 5 (eg: 25 = 5 x 5, this contributes 2 zeros),
+# repeated multiplication of 5 (eg: 25 = 5 x 5, this contributes 2 zeroes),
 # so we scale the number down by 5 (eg: 25 beomes 5) and repeat the same
 # procedure until there are no multiples of 5.
 
