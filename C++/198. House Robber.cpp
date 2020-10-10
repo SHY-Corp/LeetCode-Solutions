ce22eed3311ed6768970f8ae2325fba69dc39b9a
Steven Tang <steventango@users.noreply.github.com>
2020-10-10 10:55:53 -0600

    Improve 198. House Robber.cpp

--- C++/198. House Robber.cpp
+++ C++/198. House Robber.cpp
@@ -1,6 +1,14 @@
-/*To determine the maximum amount of money that can robbed tonight without alerting the police.
+/*198. House Robber
 
- 
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping
+you from robbing each of them is that adjacent houses have security system
+connected and it will automatically contact the police if two adjacent houses
+were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each
+house, determine the maximum amount of money you can rob tonight without
+alerting the police.
 
 For Example :
 
@@ -12,29 +20,16 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 
 class Solution {
 public:
-    int rob(vector<int>& hval) {
-       int n = hval.size();
-        if (n == 0) 
-        return 0; 
-    if (n == 1) 
-        return hval[0]; 
-    if (n == 2) 
-        return max(hval[0], hval[1]); 
-  
-    // dp[i] represent the maximum value stolen 
-    // so far after reaching house i. 
-    int dp[n]; 
-  
-    // Initialize the dp[0] and dp[1] 
-    dp[0] = hval[0]; 
-    dp[1] = max(hval[0], hval[1]); 
-  
-    // Fill remaining positions 
-    for (int i = 2; i<n; i++) 
-        dp[i] = max(hval[i]+dp[i-2], dp[i-1]); 
-  
-    return dp[n-1]; 
-        
-   
+    int rob(vector<int>& nums) {
+        int odds = 0;
+        int evens = 0;
+        for (int i = 0; i < std::size(nums); i++) {
+            if (i % 2) {
+                evens += nums[i];
+            } else {
+                odds += nums[i];
+            }
+        }
+        return std::max(odds, evens);
     }
 };
