0ce17db6f8f94cb3cb00433e630f64f3230d38f7
SharmaRajesh007 <72681707+SharmaRajesh007@users.noreply.github.com>
2020-10-12 23:30:58 +0530

    Added 1041. Robot Bounded In Circle

---
+++ Java/1041.RobotBoundedInCircle.java
@@ -0,0 +1,25 @@
+/*
+On an infinite plane, a robot initially stands at (0, 0) and faces north.  The robot can receive one of three instructions:
+
+"G": go straight 1 unit;
+"L": turn 90 degrees to the left;
+"R": turn 90 degress to the right.
+The robot performs the instructions given in order, and repeats them forever.
+
+Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+*/
+
+class Solution {
+    public boolean isRobotBounded(String ins) {
+        int x = 0, y = 0, i = 0, d[][] = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
+        for (int j = 0; j < ins.length(); ++j)
+            if (ins.charAt(j) == 'R')
+                i = (i + 1) % 4;
+            else if (ins.charAt(j) == 'L')
+                i = (i + 3) % 4;
+            else {
+                x += d[i][0]; y += d[i][1];
+            }
+        return x == 0 && y == 0 || i > 0;
+    }
+}
