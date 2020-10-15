4a2f4a2de3ce76fb2a48c3269188c0c396fbcb36
Smit Gajjar <smitgajjar.gs@gmail.com>
2020-10-16 00:05:19 +0530

    Added the problem statement and the approach used

--- C++/152. MaximumProductSubarray.cpp
+++ C++/152. MaximumProductSubarray.cpp
@@ -1,3 +1,17 @@
+// Problem:
+// Given an integer array nums, find the contiguous subarray within an array
+// (containing at least one number) which has the largest product.
+
+// Approach:
+// Since we have to care about both positive and negative product, we will
+// store two running products i.e. max_product and min_product
+// containing maximum valued and minimum valued products respectively.
+
+// Now, if a negative number is encountered, swap both the products,
+// since the sign gets changed. Otherwise, find minimum valued and maximum
+// valued subarray products ending at that index as per the standard
+// Kadane's algorithm and update the "ans" variable accordingly at each index.
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
