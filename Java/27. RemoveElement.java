011dada6c1acc3f03b293525e84ffb6cd819d98f
Peter Malast <PeterMalast@MacBook-Pro-uzivatela-Peter.local>
2020-10-15 21:49:52 +0200

    refactor

--- Java/27. RemoveElement.java
+++ Java/27. RemoveElement.java
@@ -1,22 +1,21 @@
 class Solution {
-    
+
     void delElement(int[] arr, int key, int len){
-        for(int i=key;i<len-1;i++){
-            arr[i]=arr[i+1];
-        }
+        if(arr.length < len) throw new ArrayIndexOutOfBoundsException("Length is longer than array length"); //if input value is less than array lenght, it will throw ArrayIndexOutOfBoundsException exception
+        if (len - 1 - key >= 0) System.arraycopy(arr, key + 1, arr, key, len - 1 - key); //add elements to array
     }
-    
-    
+
     public int removeElement(int[] nums, int val) {
-        int len=nums.length;
-        
-        for(int i=0 ; i < len ; i++){
-            if(nums[i]==val){
-                delElement(nums, i, len);
-                len--;
-                i--;
+        int i = 0;              //declaration variable
+        int len = nums.length;  //declaration array lenght
+
+        for(int j = 0; j < nums.length; j++){  //go through the array elements with the for loop
+            if(nums[j] != val) {       //if statement : actual value of array != input value
+                nums[i++] = nums[j];   // compare with other elements of array
+                len--;                 //decrement lenght
             }
         }
-        return len;
+        return nums.length-len;
+
     }
 }
