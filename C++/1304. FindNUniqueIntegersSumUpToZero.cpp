40632253101cddc60b9603d78a2a12e1cafa2c36
khare8871 <72224223+khare8871@users.noreply.github.com>
2020-10-18 00:09:53 +0530

    changed counter to position
    changed counter to position for easy understanding in code for vector

--- C++/1304. FindNUniqueIntegersSumUpToZero.cpp
+++ C++/1304. FindNUniqueIntegersSumUpToZero.cpp
@@ -16,17 +16,17 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> array(n,0);     //declaring a vector of integers of sie n, with each element initialised to 0
-        int counter=0;              //counter variable will keep track of where the next element will be added
+        int position=0;              //counter variable will keep track of where the next element will be added
         
         for(int i=1;i<=n/2;i++)
         {
-            array[counter]=i;
-            counter++;
+            array[position]=i;
+            position++;
         }
         for(int i=-1;i>=-n/2;i--)
         {
-            array[counter]=i;
-            counter++;
+            array[position]=i;
+            position++;
         }
         
         return array;               //since all elements were originally initialised to zero, the last element will already be 0 if n is odd
