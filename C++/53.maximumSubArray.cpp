80e26127111538bb8c3e6bc50cd677febf854ae8
sayantan1413 <sayantanbose2001@gmail.com>
2020-10-18 14:16:41 +0530

    Added question number 53

--- C++/53.maximumSubArray.cpp
+++ C++/53.maximumSubArray.cpp
@@ -1,3 +1,9 @@
+//Leetcode problem 53
+//Given an integer array nums, 
+//We have to find the contiguous subarray (containing at least one number) 
+//which has the largest sum 
+//return its sum.
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
