833ae4dd8945e3b89cf7eb8d6d8b2ffa2a0523e8
Anshuman Mishra <51750587+shivanshuman021@users.noreply.github.com>
2020-10-18 11:07:07 +0530

    Update 58. length_of_last_word.py
    Added explaination to code (approach)

--- Python/58. length_of_last_word.py
+++ Python/58. length_of_last_word.py
@@ -1,3 +1,23 @@
+'''
+PROBLEM:
+
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+'''
+
+'''
+APPROACH -
+We can convert string into list of words and can calculate length using reverse indexing
+'''
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         a = s.split()
