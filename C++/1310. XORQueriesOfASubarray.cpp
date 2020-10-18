485d78ff39bdf12b949d679ce6b690e710cbdf7b
freakcap <kulkarni.pranav123@gmail.com>
2020-10-19 03:05:57 +0530

    Proper documentation added and refactored the solution

--- C++/1310. XORQueriesOfASubarray.cpp
+++ C++/1310. XORQueriesOfASubarray.cpp
@@ -1,22 +1,50 @@
-// Given the array arr of positive integers and the array queries where queries[i] = [Li, Ri], 
-// for each query i compute the XOR of elements from Li to Ri (that is, arr[Li] xor arr[Li+1] xor ... xor arr[Ri] ). 
-// Return an array containing the result for the given queries. 
+/*
+PROBLEM - 1310. XOR Queries of a Subarray
+
+DESCRIPTION -
+
+Given the array arr of positive integers and the array queries where
+queries[i] = [Li, Ri], for each query i compute the XOR of elements from
+Li to Ri (that is, arr[Li] xor arr[Li+1] xor ... xor arr[Ri] ).
+Return an array containing the result for the given queries.
+
+CONSTRAINTS-
+
+1 <= arr.length <= 3 * 10^4
+1 <= arr[i] <= 10^9
+1 <= queries.length <= 3 * 10^4
+queries[i].length == 2
+0 <= queries[i][0] <= queries[i][1] < arr.length
+
+APPROACH-
+
+-- In this solution the property of XORs where a^(a^b)=b is exploited.
+-- First populate a prefix XOR array where array[i] is the running XOR of all
+   the elements from 0 to i.
+-- Then all the queries are answered in constant time using the above mentioned
+   property i.e.
+   XOR(l,r) = prefix(r) ^ prefix(l) = (XOR(0,l) ^ XOR(l^r)) ^ XOR(0,l)
+
+TIME COMPLEXITY-
+
+TC of solution O(n) where n is max(arr.length,queries.length)
+
+*/
 
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        int n = arr.size();
-        vector<int> ans; 
-        vector<int> seive(n,0);
-        seive[0] = arr[0];
-        
-        for(int i = 1;i<n;i++){
-            seive[i] = arr[i]^seive[i-1];     //pre-processing
+        vector<int>pref, ans; // pref is the XOR prefix, ans is answer vector
+        int pre = 0;         // variable for maintaining continuous XOR
+        for (auto& num : arr) // populating the pref array
+        {
+            pref.push_back(num ^ pre);
+            pre ^= num;
         }
-        
-        for(vector<int> qry : queries){
-            if(qry[0] == 0) ans.push_back(seive[qry[1]]);
-            else ans.push_back(seive[qry[0]-1]^seive[qry[1]]);
+        for (auto& q : queries) // answering the input queries
+        {
+            if (q[0] == 0) ans.push_back(pref[q[1]]);
+            else ans.push_back(pref[q[1]]^pref[q[0] - 1]);
         }
         return ans;
     }
