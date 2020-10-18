c138195e499cea9c8e071844a13da368038e759d
rks2607 <rishabh26jul@gmail.com>
2020-10-18 23:25:57 +0530

    Added code for Binary Tree Preorder Traversal

---
+++ C++/144. BinaryTreePreOrderTraversal.cpp
@@ -0,0 +1,33 @@
+/*Problem: Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Approach: used recursive approach to do the tree traversal
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> v;
+    
+    void traverse(TreeNode* t){
+        if(t==NULL ) return;
+        v.push_back(t->val);
+        traverse(t->left);
+        traverse(t->right);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        traverse(root);
+        return v;
+        
+    }
+};
