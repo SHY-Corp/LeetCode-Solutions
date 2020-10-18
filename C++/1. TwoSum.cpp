b899e97cafafbe3c2985b90d3241b7c1a284021f
anuj840 <70152505+anuj840@users.noreply.github.com>
2020-10-17 21:41:46 -0900

    Update 1. TwoSum.cpp

--- C++/1. TwoSum.cpp
+++ C++/1. TwoSum.cpp
@@ -19,3 +19,22 @@ public:
         return {};   // return empty vector if condition fails for every element
     }
 };
+//Complete code 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        vector<int> v;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                return v;
+            }
+            m[nums[i]]=i;
+        }
+        return v;
+    }
+};
