a8b4ed0ba751a1f7bfdc2871e2d7886cab7b12ce
Ryuk6666 <62832135+Ryuk6666@users.noreply.github.com>
2020-10-18 13:20:47 +0530

    Create inserting_at_end.py

---
+++ Python/inserting_at_end.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+# Function to add newnode
+    def AtEnd(self, newdata):
+        NewNode = Node(newdata)
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        laste = self.headval
+        while(laste.nextval):
+            laste = laste.nextval
+        laste.nextval=NewNode
+
+# Print the linked list
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtEnd("Thu")
+
+list.listprint()
