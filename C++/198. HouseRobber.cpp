a05f8bd95d1cd291d40c78ceeedd44f0f1d55648
Piyush228 <45069778+Piyush228@users.noreply.github.com>
2020-10-20 13:44:33 +0530

    Update 198. HouseRobber.cpp

--- C++/198. HouseRobber.cpp
+++ C++/198. HouseRobber.cpp
@@ -17,8 +17,7 @@ Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
 */
-
-class Solution {
+/*class Solution {
 public:
     int rob(vector<int>& nums) {
         int odds = 0;
@@ -31,5 +30,31 @@ public:
             }
         }
         return std::max(odds, evens);
+    }
+};*/
+// this code note work in if nums=[2,1,1,2]  your output was come 3 and expected output was 4
+//below code was right
+class Solution {
+public:
+   int rob(vector<int>& nums) {
+        int n=nums.size();
+          if(n==0)
+            return 0;
+          if(n==1)
+              return nums[0];
+          if(n==2)
+              return max(nums[0],nums[1]);
+         int t[n];
+         t[0]=nums[0];
+       t[1]=max(nums[0],nums[1]);
+       for(int i=2;i<n;i++)
+       {
+           t[i]=max(nums[i]+t[i-2],t[i-1]);
+       }
+       for(int i=0;i<n;i++)
+           cout<<t[i]<<" ";
+         cout<<endl;
+              
+        return t[n-1];
     }
 };
