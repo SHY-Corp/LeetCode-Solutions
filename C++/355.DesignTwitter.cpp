391721e023bfe3723472b3260f62f51c6b9d08d8
Divya S <divyasaravanan0327@gmail.com>
2020-10-21 13:22:03 +0530

    Adding Design Twitter

--- C++/355.DesignTwitter.cpp
+++ C++/355.DesignTwitter.cpp
@@ -1,3 +1,13 @@
+/*Question :
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+
+postTweet(userId, tweetId): Compose a new tweet.
+getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+follow(followerId, followeeId): Follower follows a followee.
+unfollow(followerId, followeeId): Follower unfollows a followee.
+
+*/
+
 class Twitter {
 public:
     /** Initialize your data structure here. */
