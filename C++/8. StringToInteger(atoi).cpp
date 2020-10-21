4765a3418a5161ba07806fb644b5dd264075e16e
Drew Michelini <dbmich@umich.edu>
2020-10-21 10:44:46 -0400

    Added code for 28 | String to Integer (atoi)

--- C++/8. StringToInteger(atoi).cpp
+++ C++/8. StringToInteger(atoi).cpp
@@ -1,3 +1,13 @@
+/*Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned. */
+
 #include <cctype>
 #include <limits>
 class Solution {
