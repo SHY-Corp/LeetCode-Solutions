f6415c1cd74ea375b4ceab2dfbc9c687d72b0909
Franco Colman <colmanfm@gmail.com>
2020-10-21 09:04:45 +0200

    [ADD] Added comments on the solution explaining what the program does.

--- Java/1050.PrimeFactors.java
+++ Java/1050.PrimeFactors.java
@@ -1,8 +1,12 @@
+/**
+ *Implemented a basic program to calculate the prime factors of a given number (see prime factors kata).
+ *The given number should be just an integer and the program would return a list of the prime factors of the given integer.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution
-{
+class Solution {
     public static List<Integer> factorsOf(int n)
     {
         ArrayList<Integer> factors = new ArrayList<>();
