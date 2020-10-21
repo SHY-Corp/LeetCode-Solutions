ea69fa3b1946e1715476248ba6b8e61fde0c4ce9
Sumit Kumar Gupta <61278456+sumitgupta7132@users.noreply.github.com>
2020-10-21 13:04:37 +0530

    Update 231.PowerOfTwo.cpp

--- C++/231.PowerOfTwo.cpp
+++ C++/231.PowerOfTwo.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        // (n>0) means that the number is not zero 
+        // a number which is power of two has one set bit so !x&(x-1) return 1 if the number is power of two
         return (n>0)&&(n&&!(n&(n-1)));
-
+        // 8- 1000
+        // 7- 0111
+        //&  !0000 = 1111 so 8 is power of two
     }
 };
