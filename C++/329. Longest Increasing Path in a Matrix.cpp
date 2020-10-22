13f61fb1f120086ac270b887bc9ca7f3335091a1
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2020-10-22 19:54:27 +0530

    Update 329. Longest Increasing Path in a Matrix.cpp

--- C++/329. Longest Increasing Path in a Matrix.cpp
+++ C++/329. Longest Increasing Path in a Matrix.cpp
@@ -1,3 +1,10 @@
+/*
+Given an integer matrix, find the length of the longest increasing path.
+
+From each cell, you can either move to four directions: left, right, up or down.
+You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is 
+not allowed).
+*/
 class Solution {
 public:
     int dp[1005][1005],n,m;
