33c5de6fd381eaf0d6e8b85fdaa30d8d002e66bf
Pransh Tiwari <tpransh@amazon.com>
2020-10-22 13:17:19 +0530

    Added C++ solution to 617. MergeTwoBinaryTrees

---
+++ C++/617. MergeTwoBinaryTrees.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1)
+            return t2;
+        if(!t2)
+            return t1;
+        TreeNode* t = new TreeNode(t1->val + t2->val);
+        t->left = mergeTrees(t1->left, t2->left);
+        t->right = mergeTrees(t1->right, t2->right);
+        return t;
+    }
+};
