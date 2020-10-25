342ac03160d9dfa14de4102431a3599ce41953df
Nithin R <37147336+thebinarybot@users.noreply.github.com>
2020-10-25 18:04:43 +0530

    Update and rename 39.combinationSum.py to 39. combinationSum.py

--- Python/39.combinationSum.py
+++
@@ -1,7 +0,0 @@
-  class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[[]]] + [[] for _ in range(target)]
-        for candidate in candidates:
-            for i in range(candidate,target+1):
-                dp[i] += [sublist + [candidate] for sublist in dp[i-candidate]]
-        return dp[target]
