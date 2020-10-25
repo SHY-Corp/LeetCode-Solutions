84307d72c0f1372c5a982fec59e86859032604f1
Dilpreet Kaur <73196093+h20190907@users.noreply.github.com>
2020-10-25 09:03:05 +0530

    Added Title and Problem Statement for #876

--- C++/875. KokoEatingBananas.cpp
+++ C++/875. KokoEatingBananas.cpp
@@ -1,3 +1,23 @@
+/* Problem #875 "KoKo Eating bananas" Link: https://leetcode.com/problems/koko-eating-bananas/
+Problem Statement : Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  
+The guards have gone and will come back in H hours.
+
+Koko can decide her bananas-per-hour eating speed of K.  
+Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  
+If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
+
+Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
+
+Return the minimum integer K such that she can eat all the bananas within H hours.
+
+Example: 
+Input: piles = [3,6,7,11], H = 8
+Output: 4
+*/
+
+
+
+
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int H) {
