a3991863776c104401f77920a7ec67276585c402
Pransh Tiwari <tpransh@amazon.com>
2020-10-26 01:27:34 +0530

    Added the comments for explaining the merge of trees

--- C++/617. MergeTwoBinaryTrees.cpp
+++ C++/617. MergeTwoBinaryTrees.cpp
@@ -12,10 +12,14 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        // Return the other tree, if either of the tree is null.
         if(!t1)
             return t2;
         if(!t2)
             return t1;
+
+        /* Create a new node, with value as the summation of both the nodes. Attach the left and right
+        children and recur for them. */
         TreeNode* t = new TreeNode(t1->val + t2->val);
         t->left = mergeTrees(t1->left, t2->left);
         t->right = mergeTrees(t1->right, t2->right);
