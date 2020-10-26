a5c9bfde4c1e7e58df12e4800e6a8d74897ac34e
Natalia Gutierrez <ngutierrez131@gmail.com>
2020-10-26 01:31:49 -0400

    Fixing tabbing

--- Python/1119. RemoveVowelFromString.py
+++ Python/1119. RemoveVowelFromString.py
@@ -3,12 +3,12 @@
 # Example: given 'Hello world', the program returns 'Hll wrld'
 
 class Solution(object):
-   def removeVowelsFromString(self, string):
-       vowels = ('a','e','i','o','u')
-       new_string = ""
+    def removeVowelsFromString(self, string):
+        vowels = ('a','e','i','o','u')
+        new_string = ""
 
-       for s in string:
-           if s.lower() not in vowels:
-               new_string += s
+        for s in string:
+            if s.lower() not in vowels:
+                new_string += s
 
         return new_string
