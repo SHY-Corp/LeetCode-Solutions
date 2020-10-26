add4af0996dcdb49f1e3277081c67298d5f4e02b
Natalia Gutierrez <ngutierrez131@gmail.com>
2020-10-26 01:23:18 -0400

    adding description

--- Python/1119. RemoveVowelFromString.py
+++ Python/1119. RemoveVowelFromString.py
@@ -1,10 +1,14 @@
+# 1119. This program removes the vowels from a given string
+#
+# Example: given 'Hello world', the program returns 'Hll wrld'
+
 class Solution(object):
-   def numJewelsInStones(self, string):
+   def removeVowelsFromString(self, string):
        vowels = ('a','e','i','o','u')
        new_string = ""
 
        for s in string:
            if s.lower() not in vowels:
                new_string += s
-               
+
         return new_string
