613926d246a23b80cd9a1f70a3f99c09bc39fe5e
rohitgeddam <48797475+rohitgeddam@users.noreply.github.com>
2020-10-27 15:47:11 +0530

    Create RotateImage.java
    Solves Leetcode #48 (Rotate Image problem)
    By using the relation "matrix[i][j] = matrix[n-1-j][i]", we can loop through the matrix.

---
+++ Java/RotateImage.java
@@ -0,0 +1,12 @@
+public void rotate(int[][] matrix) {
+	int n = matrix.length;
+	for (int i = 0; i < n / 2; i++) {
+		for (int j = 0; j < Math.ceil(((double) n) / 2.); j++) {
+			int temp = matrix[i][j];
+			matrix[i][j] = matrix[n-1-j][i];
+			matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+			matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+			matrix[j][n-1-i] = temp;
+		}
+	}
+}
