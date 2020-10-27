8c207220c8aa05e9ba3de167eec53ee3fa789465
GHANENDER CHAUHAN <72665196+ghanender-chauhan@users.noreply.github.com>
2020-10-27 15:24:55 +0530

    Integer TO Roman

---
+++ Python/Integer TO Roman.py
@@ -0,0 +1,26 @@
+class py_solution:
+    def int_to_Roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+
+print(py_solution().int_to_Roman(10))
+print(py_solution().int_to_Roman(550))
