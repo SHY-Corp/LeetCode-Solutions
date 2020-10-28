d98348c2d19a66d965b071f48ca35f76242660a2
Priya Aggarwal <46966775+Priya098098@users.noreply.github.com>
2020-10-28 19:58:53 +0530

    Update 1. TwoSum.cpp

--- C++/1. TwoSum.cpp
+++ C++/1. TwoSum.cpp
@@ -3,16 +3,24 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-class Solution {
+class Solution 
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
         
-        for(int i=0; i<nums.size(); i++) {            //outer loop  0 --> size of vector nums
+        for(int i=0; i<nums.size(); i++)   //outer loop  0 --> size of vector nums
             
-            for(int j=1; j<nums.size(); j++) {        //inner loop  1 --> size of vector nums
+        {            
+            
+            for(int j=1; j<nums.size(); j++)  // inner loop 1 --> size of vectors nums
+            {        
                 
-                if(nums[i]+nums[j]==target && i!=j)   
-                    return vector<int> {i,j};         // return vector of i and j if condition satisfies
+                if(nums[i]+nums[j]==target && i!=j) 
+                {
+                    return vector<int> {i,j};  // return vector of i and j if condition satisfies
+                    
+                }
                     
             }
         }
