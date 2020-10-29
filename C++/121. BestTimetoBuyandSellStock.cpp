653aa81b85e0062a20b93af519d44346a0994714
Harsh Agarwal <44464398+AgHarsh@users.noreply.github.com>
2020-10-29 13:40:32 +0530

    Update 121. BestTimetoBuyandSellStock.cpp
     A better solution

--- C++/121. BestTimetoBuyandSellStock.cpp
+++ C++/121. BestTimetoBuyandSellStock.cpp
@@ -5,22 +5,18 @@
 // design an algorithm to find the maximum profit.
 // Note that you cannot sell a stock before you buy one.
 
-// Approach:
-// Initialize mn variable to first value. Now, find the running difference between current
-// value and mn variable, since that would give us the required maximum profit.
-
 class Solution {
 public:
-	int maxProfit(vector<int>& prices) {
-		int n = prices.size();
-		if (!n) return 0;
-		int mn = prices[0], ans = 0;
-		for (int i = 1; i < n; i++)
-		{
-			ans = max({0, ans, prices[i] - mn});
-			if (prices[i] < mn)
-				mn = prices[i];
-		}
-		return ans;
-	}
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int n=prices.size();
+        if(n==0 || n==1)
+            return profit;
+        int min_price=prices[0];
+        for(int i=0;i<n;i++){
+            min_price=min(min_price,prices[i]);
+            profit=max(prices[i]-min_price,profit);
+        }
+        return profit;                    
+    }
 };
