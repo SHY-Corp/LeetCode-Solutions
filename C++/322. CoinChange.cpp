b32a604a8065bc1640281028610957b205f577af
Harsh Agarwal <44464398+AgHarsh@users.noreply.github.com>
2020-10-29 13:42:32 +0530

    Update 322. CoinChange.cpp
    A short and better solution

--- C++/322. CoinChange.cpp
+++ C++/322. CoinChange.cpp
@@ -8,26 +8,18 @@ coins = [1,2,5], amount = 11
 Output : 3
 Explanation : 11 = 5 + 5 + 1
 """
-
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int t[coins.size()+1][amount+1];
-        for(int i=0;i<=coins.size();i++){
-            t[i][0] = 0;//If the amount of money, we require no coin to distribute it. So, the answer is 0.
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int n=prices.size();
+        if(n==0 || n==1)
+            return profit;
+        int min_price=prices[0];
+        for(int i=0;i<n;i++){
+            min_price=min(min_price,prices[i]);
+            profit=max(prices[i]-min_price,profit);
         }
-        for(int i=0;i<=amount;i++){
-            t[0][i] = INT_MAX-1;//If the amount of money is non-zero and we do not have any coins of a particular denomination,we can't partition it, in other words,there will be infinite number of coins required.
-        }
-        for(int i=1;i<=coins.size();i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j){//If we can use that coin
-                    t[i][j] = min(1+t[i][j-coins[i-1]],t[i-1][j]);
-                }else{//If we cannot use it
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return (t[coins.size()][amount] >= 2147483646)?-1:t[coins.size()][amount];
+        return profit;                    
     }
 };
