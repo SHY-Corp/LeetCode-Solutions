d71c669794872c61cef7b6f44964d09ecd10ee89
Varian Caesar <varian.caesar@dekoruma.com>
2020-10-29 14:01:48 +0700

    Add problem description

--- Python/1221. SplitAStringInBalancedStrings.py
+++ Python/1221. SplitAStringInBalancedStrings.py
@@ -1,6 +1,17 @@
 '''
 Problem link: https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
+
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+Given a balanced string s split it in the maximum amount of balanced strings.
+Return the maximum amount of splitted balanced strings.
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
 idea:
 1. Initialize a counter variable and result variable: count, result
 2. Loop from left to right
