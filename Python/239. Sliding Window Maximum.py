16e834f378142c298a4955d32e8c95ef282d72da
Shantanu Kale <65826354+SSKale1@users.noreply.github.com>
2020-10-30 21:04:42 +0530

    Update and rename 239.Sliding Window Maximum.py to 239. Sliding Window Maximum.py

--- Python/239. Sliding Window Maximum.py
+++ Python/239. Sliding Window Maximum.py
@@ -1,20 +1,19 @@
-// Problem - 239. Sliding Window Maximum 
+""" Problem - 239. Sliding Window Maximum 
 
-/*
 Problem statement -
 You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 Return the max sliding window.
-*/
 
-/*
+
+
 Constraints - 
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
 1 <= k <= nums.length
-*/
 
-/*
+
+
 Example 1:
 
 Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
@@ -45,10 +44,9 @@ Example 5:
 Input: nums = [4,-2], k = 2
 Output: [4]
 
-*/
+
 
 //Solution
-/*
 Idea -
 Keep indexes of good candidates in deque d. The indexes in d are from the current window, they're increasing, and their corresponding nums are decreasing. Then the first deque element is the index of the largest window value.
 
@@ -58,7 +56,7 @@ For each index i:
 2.Append the current index.
 3.Pop (from the front) the index i - k, if it's still in the deque (it falls out of the window).
 4.If our window has reached size k, append the current window maximum to the output.
-
+"""
 
 
 class Solution:
