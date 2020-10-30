27e343a2c3fc0865110e384bf91cb418d10bf555
vivaswat19 <54148533+vivaswat19@users.noreply.github.com>
2020-10-30 23:18:21 +0530

    adding question details

--- C++/506. Relative Ranks.cpp
+++ C++/506. Relative Ranks.cpp
@@ -1,3 +1,13 @@
+/*
+Given scores of N athletes, find their relative ranks and the people with the top three highest scores, 
+who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+
+Input: [5, 4, 3, 2, 1]
+Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+Explanation: The first three athletes got the top three highest scores, so they got "Gold Medal", "Silver Medal" and "Bronze Medal". 
+For the left two athletes, you just need to output their relative ranks according to their scores.
+*/
+
 class Solution {
 public:
     static bool sortcol(const vector<int> &v1,const vector<int> &v2) {
