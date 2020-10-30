b504b1bd1e080f10e0a5bdada7ec64466ec78564
Sommano Phivilay <sommano@gmail.com>
2020-10-30 17:09:10 -0500

    Create 28. ImplementstrStr.py

---
+++ Python/28. ImplementstrStr.py
@@ -0,0 +1,23 @@
+"""
+This program returns the index of the first occurrence of needle in haystack, or -1 if
+needle is not part of haystack.
+"""
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        n = len(haystack)
+        m = len(needle)
+        for i in range(n + 1 - m):
+            matched = True
+            for k in range(m):
+                if haystack[i + k] != needle[k]:
+                    matched = False
+                    break
+            if matched:
+                return i
+        return -1
