48eed95e9fdf62a5cf25e30c2a41e0f6cce12b98
unknown <shouvikch1997@gmail.com>
2020-10-30 21:30:50 +0530

    Added 3 Sum in Java

---
+++ Java/15.3 Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) return new ArrayList<>(); // if nums less than 3 element
+        Arrays.sort(nums); // sort array
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) set.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                else if (sum > 0) k--;
+                else if (sum < 0) j++;
+            }
+
+        }
+
+        return new ArrayList<>(set);
+    }
+}
