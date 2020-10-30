65921f743aafe7d591fbdd567407ed3503d4240a
Jagjit Singh <jagjitsingh@Jagjits-MacBook-Pro.local>
2020-10-30 12:16:13 +0530

    Adding solution to 749. Contain Virus

--- Java/749. ContainVirus.java
+++ Java/749. ContainVirus.java
@@ -1,3 +1,34 @@
+// A virus is spreading rapidly, and your task is to quarantine the infected area by installing walls.
+
+// The world is modeled as a 2-D array of cells, where 0 represents uninfected cells, and 1 represents cells contaminated with the virus. A wall (and only one wall) can be installed between any two 4-directionally adjacent cells, on the shared boundary.
+
+// Every night, the virus spreads to all neighboring cells in all four directions unless blocked by a wall. Resources are limited. Each day, you can install walls around only one region -- the affected area (continuous block of infected cells) that threatens the most uninfected cells the following night. There will never be a tie.
+
+// Can you save the day? If so, what is the number of walls required? If not, and the world becomes fully infected, return the number of walls used.
+
+
+//Example 1
+
+// Input: grid = 
+// [[0,1,0,0,0,0,0,1],
+//  [0,1,0,0,0,0,0,1],
+//  [0,0,0,0,0,0,0,1],
+//  [0,0,0,0,0,0,0,0]]
+// Output: 10
+// Explanation:
+// There are 2 contaminated regions.
+// On the first day, add 5 walls to quarantine the viral region on the left. The board after the virus spreads is:
+
+// [[0,1,0,0,0,0,1,1],
+//  [0,1,0,0,0,0,1,1],
+//  [0,0,0,0,0,0,1,1],
+//  [0,0,0,0,0,0,0,1]]
+
+// On the second day, add 5 walls to quarantine the viral region on the right. The virus is fully contained.
+
+//Approach
+//Using dfs traversel using graph
+
 class Solution {
     private int[][] dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     private class Count{
