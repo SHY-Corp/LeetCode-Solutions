d5236c9a331fabb1170f2ea288add0f6e94f6568
Shuvam Shah <44120980+shvms@users.noreply.github.com>
2020-10-31 13:00:50 +0530

    Comments added to 983. MinimumCostForTickets.java

--- Java/983. MinimumCostForTickets.java
+++ Java/983. MinimumCostForTickets.java
@@ -1,21 +1,22 @@
 class Solution {
   public int mincostTickets(int[] days, int[] costs) {
+    // creating a dp table for all days in a year
     int[] dp = new int[365 + 1];
-    dp[0] = 0;
+    dp[0] = 0;  // base case
     int p = 0;
 
     for (int i = 1; i < dp.length; ++i) {
-      dp[i] = dp[i - 1];
+      dp[i] = dp[i - 1];  // default scenario
 
       if (p < days.length && i == days[p]) {
-        dp[i] = dp[i] + costs[0];
+        dp[i] = dp[i] + costs[0];   // dp case when buying ticket for a single day pass
 
-        if (i - 7 > 0)
+        if (i - 7 > 0)    // dp case when buying ticket for 7 days
           dp[i] = Integer.min(dp[i - 7] + costs[1], dp[i]);
         else
           dp[i] = Integer.min(dp[0] + costs[1], dp[i]);
 
-        if (i - 30 > 0)
+        if (i - 30 > 0)   // dp case when buying ticket for a month (30 days)
           dp[i] = Integer.min(dp[i - 30] + costs[2], dp[i]);
         else
           dp[i] = Integer.min(dp[0] + costs[2], dp[i]);
