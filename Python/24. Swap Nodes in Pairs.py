b247631f6771c10ece208555810324ad113bad64
Kajal <kajalpuri95@gmail.com>
2020-11-01 04:52:27 +0100

    Update 24. Swap Nodes in Pairs.py

--- Python/24. Swap Nodes in Pairs.py
+++ Python/24. Swap Nodes in Pairs.py
@@ -4,7 +4,16 @@
 #         self.val = val
 #         self.next = next
 
-        
+#Approach :
+
+'''
+Put Node2.next which is n3->n4->...nn into Node1.next
+Now put entire Node1 which is n1-n3-n4... into Node2.next . with this step n1 and n2 are swapped.
+In the first iteration save the pointer of new 2nd node  which is node1. This pointer node keeps the link between Pari1..Pari2...Pair3....etc.
+From the second iteration onwards, swap and link the first node from the pair to the second node from last iteration.
+Move the pointer 2nd node of the pair.
+'''
+
 class Solution(object):
     
     def swapPairs(self, head: ListNode) -> ListNode:
@@ -25,22 +34,18 @@ class Solution(object):
             Node2 = tempNode.next
             tempNode= tempNode.next.next
 
-            #put Node2.next which is n3->n4->...nn into Node1.next    
+                
             Node1.next=Node2.next
-            #Now put entire Node1 which is n1-n3-n4... into Node2.next . with this step n1 and n2 are swapped.
             Node2.next=Node1
             
             if first==0:
-                # In the first iteration save the pointer of new 2nd node  which is node1
-                # This pointer node keeps the link between Pari1..Pari2...Pair3....etc.
+                
                 pointerNode= Node1
                 first=1
+                
             else:
-                """ from the second iteration onwards, swap and link the first
-                    node from the pair to the second node from last iteration
-                """
+                
                 pointerNode.next=Node2
-                """ Move the pointer 2nd node of the pair"""
                 pointerNode=Node1
                 
         return newheadNode
