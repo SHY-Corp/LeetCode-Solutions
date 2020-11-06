ffc3d731adc459ba8eb0e0bf9cea76195ec6ac71
juhikushwah <43586452+juhikushwah@users.noreply.github.com>
2020-11-06 11:03:59 -0800

    Update 206.ReverseLinkedList.cpp

--- C++/206.ReverseLinkedList.cpp
+++ C++/206.ReverseLinkedList.cpp
@@ -5,12 +5,25 @@ public:
         if(head == NULL)
             return NULL;
         
+        /*I created 3 pointers. prev points to null and 
+          the remaining 2 point to head. 
+        */
         ListNode* prev=NULL;
         ListNode* curr=head;
         ListNode* temp=head;
         
+        /* I am traversing it till the current element reaches 
+           null and applying operations to swap values.
+        */
         while(curr!=NULL)
         {
+            
+         /*  First, I shift temp from head to next element. 
+             Then my current node's address points to previous
+             instead of next. Now, my prev is equal to the current
+             element and current is equal to temp. In the end, I return
+             prev which is the reversed linked list.
+         */
             temp=temp->next;
             curr->next=prev;
             prev=curr;
