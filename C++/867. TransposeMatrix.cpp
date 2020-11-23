e7693e5ac73edaf0038d15a13751fe236f47df5d
Saurav <you@example.comjsrana@LAPTOP-O615J2RF.(none)>
2020-11-23 10:37:43 +0530

    Saurav added

--- C++/867. TransposeMatrix.cpp
+++ C++/867. TransposeMatrix.cpp
@@ -1,18 +1,19 @@
 // Problem - 867. Transpose Matrix
+// This code will help you in Transposing any Square Matrix(n*n).
 
 class Solution {
     public:
-    int n;
+    int n;                      // Taking input as a number for the size of matix.
     cin>>n;
     
     int a[n][n];
-    for(int i=0 ;i<n; i++){
+    for(int i=0 ;i<n; i++){     
         for(int j=0; j<n; j++){
             cin>>a[i][j];
         }
     }
 
-    for(int i=0 ;i<n; i++){
+    for(int i=0 ;i<n; i++){     // It will show you the Original matrix that you have inserted.
         for(int j=0; j<n; j++){
             cout<<" "<<a[i][j];
         }
@@ -20,7 +21,7 @@ class Solution {
     }
     cout<<endl;
 
-    for(int i=0 ;i<n; i++){
+    for(int i=0 ;i<n; i++){     // This is the Main Logic behind for Transposing Matrix.
         for(int j=i; j<n; j++){
             // swap
             int temp = a[i][j];
@@ -29,7 +30,7 @@ class Solution {
         }
     }
 
-    for(int i=0 ;i<n; i++){
+    for(int i=0 ;i<n; i++){     // It will give the final output Transposed Matrix.
         for(int j=0; j<n; j++){
             cout<<" "<<a[i][j];
         }
