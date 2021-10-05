0d37940456f50a8ce9d792dc6ef426ebbe16a021
Jonas Kühn <17148798+JOKUE2002@users.noreply.github.com>
2021-10-05 23:03:05 +0200

    Update and rename 175. Combine two Tables.sql to 175. CombineTwoTables.sql

--- SQL/175. CombineTwoTables.sql
+++ SQL/175. CombineTwoTables.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+# The query selects Names and joins the Address table to the Person table
 select p.Firstname, p.LastName, a.City, a.State from Person p left join Address a on p.PersonId = a.PersonId;
