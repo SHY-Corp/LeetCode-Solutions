710e98290f2f95b56e00437cb8d9583322ba1eea
Tales Alves <5470324-tta13@users.noreply.gitlab.com>
2021-10-31 10:52:35 -0300

    Adding relevant comments explaining the solution

--- Python/66. PlusOne.py
+++ Python/66. PlusOne.py
@@ -9,8 +9,12 @@ Increment the large integer by one and return the resulting array of digits.
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+	# Transforming the input array into a list of characters
         strs = [str(digit) for digit in digits]
+	# Joining the characters into a string
         string = "".join(strs)
+	# Transforming the string into an int and adding 1 to it
         value = int(string)
         value += 1
+	# Converting the int back to an array and returning
         return [int(x) for x in str(value)]
